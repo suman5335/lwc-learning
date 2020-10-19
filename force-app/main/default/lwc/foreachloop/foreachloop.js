1f6b9455aa6378dc4e3c3d9b1477d4f11502d196
balusu_suman <sumanbalusu9@gmail.com>
2020-10-19 08:52:39 +0530

    19/10 class- foreach and iteration

---
+++ force-app/main/default/lwc/foreachloop/foreachloop.js
@@ -0,0 +1,21 @@
+import { LightningElement } from 'lwc';
+
+export default class Foreachloop extends LightningElement {
+    objlist=[{
+        id:101 ,
+        name:"A",
+        skill:"X"
+    }, 
+    {
+        id:102,
+        name:"B",
+        skill:"y"
+    },
+    {
+        id:103,
+        name:"C",
+        skill:"Z"
+    
+    }]
+    
+}
