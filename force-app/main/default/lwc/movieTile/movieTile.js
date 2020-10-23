58f6b26374bada4b36635f66e7ae59cddc924c4a
balusu_suman <sumanbalusu9@gmail.com>
2020-10-23 23:08:08 +0530

    movieTile

--- force-app/main/default/lwc/movieTile/movieTile.js
+++ force-app/main/default/lwc/movieTile/movieTile.js
@@ -1,3 +1,106 @@
 import { LightningElement } from 'lwc';
 
-export default class MovieTile extends LightningElement {}
+export default class MovieList extends LightningElement {
+    sushantMovies=[
+        {
+        movieName:'Dil Bechara',
+        director:'Mukesh Chhabra',
+        stars:' Sushant Singh Rajput, Sanjana Sanghi, Swastika Mukherjee',
+        movierating:'*****',
+        releaseDate:'24 July 2020;',
+        bannerImg: 'https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2018/the-fault-in-our-stars-remake/poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/dil-bechara-official-trailer'
+        },
+        {
+        movieName:'Drive',
+        director:'Tarun Mansukhani',
+        stars:' Sushant Singh Rajput , Jacqueline Fernandez, Vikramjeet Virk',
+        movierating:'*****',
+        releaseDate:'01 November, 2019',
+        bannerImg: 'https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2017/drive/poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/drive-official-trailer'
+        },
+       {
+        movieName: 'Chhichhore',
+        director:'Nitesh Tiwari',
+        stars:'Sushant Singh Rajput , Shraddha Kapoor, Varun Sharma',
+        movierating:'*****',
+        releaseDate:'06 September, 2019',
+        bannerImg: 'https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2019/nitesh-tiwari-s-next/poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/chhichhore-dosti-special-trailer'
+        },
+        {
+        movieName: 'Sonchiriya',
+        director:' Abhishek Chaubey',
+        stars:' Sushant Singh Rajput , Bhumi Pednekar, Manoj Bajpai',
+        movierating:'*****',
+        releaseDate:'01 March, 2019',
+        bannerImg: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/46/Sonchiriya_poster.jpg/220px-Sonchiriya_poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/the-rebels-of-sonchiriya'
+        },
+        
+        {
+        movieName: 'Kedarnath',
+        director:'Abhishek Kapoor',
+        stars:'Sushant Singh Rajput , Sara Ali Khan, Nitish Bharadwaj',
+        movierating:'*****',
+        releaseDate:'07 December, 2018',
+        bannerImg: 'https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2018/kedarnath/poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/kedarnath-official-trailer'
+        },
+        {
+        movieName: 'Raabta',
+        director:'Dinesh Vijan',
+        stars:'Sushant Singh Rajput , Kriti Sanon, Jim Sarbh',
+        movierating:'*****',
+        releaseDate:'09 June, 2017',
+        bannerImg: 'https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2016/raabta/poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/raabta-official-trailer-sushant-singh-rajput-kriti-sanon'
+        },
+        {
+        movieName: 'M. S. Dhoni - The Untold Story',
+        director:'Neeraj Pandey',
+        stars:'Sushant Singh Rajput , Kiara Advani, Disha Patan',
+        movierating:'*****',
+        releaseDate:'30 September, 2016',
+        bannerImg: 'https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2015/m-s-dhoni-the-untold-story/poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/m-s-dhoni-the-untold-story-official-trailer-sushant-singh-rajput'
+        },
+        {
+        movieName:'Detective Byomkesh Bakshy!',
+        director:'Dibakar Banerjee',
+        stars:'Sushant Singh Rajput , Anand Tiwari, Swastika Mukherjee',
+        movierating:'*****',
+        releaseDate:'03 April, 2015',
+        bannerImg: 'https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2014/detective-byomkesh-bakshi/poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/2nd-official-trailer-of-detective-byomkesh-bakshy'
+        },
+        {
+        movieName:'Pk',
+        director:'Rajkumar Hirani',
+        stars:'Aamir Khan , Anushka Sharma, Sushant Singh Rajput',
+        movierating:'*****',
+        releaseDate:'03 April, 2015',
+        bannerImg: 'https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2013/peekay/poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/the-official-teaser-of-pk'
+        },
+        {
+        movieName:'Shuddh Desi Romance',
+        director:'Maneesh Sharma',
+        stars:'Sushant Singh Rajput , Parineeti Chopra, Vaani Kapoor',
+        movierating:'*****',
+        releaseDate:'06 September, 2013',
+        bannerImg: 'https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2013/shuddh-desi-romance/poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/the-official-trailer-of-shuddh-desi-romance'
+        },
+        {
+        movieName:'Kai Po Che',
+        director:'Abhishek Kapoor',
+        stars:'Sushant Singh Rajput , Amit Sadh, Rajkummar Rao',
+        movierating:'*****',
+        releaseDate:'22 February, 2013',
+        bannerImg: 'https://cdn.bollywoodmdb.com/movies/largethumb/250x267/2012/kai-po-che/poster.jpg',
+        trailer:'https://www.bollywoodmdb.com/movies/broadband/kai-po-che-theatrical-trailere'
+        }
+    ];
+}
